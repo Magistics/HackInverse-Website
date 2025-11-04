8887933c587b0a4d59e09c8376104a44cef12384
ATRIKA SHOW <readeratrika08@gmail.com>
2025-11-04 23:18:05 +0530

    initialize React project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <div className='bg-black text-white w-screen h-screen flex justify-center items-center'>
+    <div>HackInverse</div>
+   </div>
+   </>
+  )
+}
+
+export default App
