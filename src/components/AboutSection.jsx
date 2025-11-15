402e3548004c07d15e34afac1fa942c271014607
Hiya Sarkar <sarkarhiya53@gmail.com>
2025-11-15 22:59:10 +0530

    Calender and aboutsection modified

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,8 +10,8 @@ import {
 
 const AboutSection = () => {
   return (
-    <div>
-      <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-6xl text-center font-serif mt-10 sm:mt-16 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
+    <div className="sm:mt-15 md:mt-30">
+      <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-7xl text-center font-serif mt-10 sm:mt-10 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
       <span className="text-white mr-3 md:mr-5">About</span>
       <span className="inline-block">HACKINVERSE</span>
     </h2>
@@ -19,7 +19,7 @@ const AboutSection = () => {
         className="after:content-[''] after:block after:h-[1.5px] after:w-full after:bg-red-600 after:mt-1 before:content-[''] before:block before:h-[1.5px] before:w-full before:bg-red-600 mx-4 sm:mx-8 md:mx-20 lg:mx-100"
       ></div>  
 
-      <p className="flex items-center text-white justify-center mt-6 sm:text-lg md:text-base font-mono max-w-3xl mx-auto text-center md:text-left">
+      <p className="flex items-center text-white justify-center mt-13 sm:text-xs md:text-xl font-mono max-w-3xl mx-auto text-center md:text-left">
         HackInverse is the flagship hackathon by Magistics, built to bring
         together students, developers, designers and innovators from all walks
         of life. It's a community-driven space where creativity meets
@@ -28,8 +28,8 @@ const AboutSection = () => {
 
       <wbr></wbr>
 
-      <div className=" py-16 px-6">
-        <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between ">
+      <div className=" py-16 px-15">
+        <div className="max-w-3xl mx-auto flex flex-col md:flex-row items-center justify-between ">
           <div className="md:w-1/2 flex justify-center">
             <img
               src="./src/assets/monster.png"
@@ -39,7 +39,7 @@ const AboutSection = () => {
           </div>
 
           <div className="md:w-1/2 flex flex-col items-center md:items-start text-center md:text-right">
-            <p className="text-lg text-white leading-relaxed max-w-md font-mono">
+            <p className="sm:text-xs md:text-xl text-white leading-relaxed max-w-md font-mono">
               From brainstorming to building,
               <br />
               HackInverse inspires
@@ -53,7 +53,7 @@ const AboutSection = () => {
               innovations.
             </p>
 
-            <div className="flex gap-4 mt-6 justify-center md:justify-start md:ml-20">
+            <div className="flex gap-4 mt-6 justify-center md:justify-start md:ml-30">
               <Link
                 ahref="https://www.facebook.com/share/1DBsk7vayo/"
                 value={<FaFacebookF />}
@@ -73,7 +73,7 @@ const AboutSection = () => {
               />
             </div>
 
-            <button className="flex items-center space-x-2 mt-6 md:ml-40 cursor-pointer bg-red-600 hover:bg-red-700 text-white font-mono px-4 py-2 rounded-full shadow-lg transition hover:scale-110 hover:shadow-[0_0_30px_#ff0000dd]">
+            <button className="flex items-center space-x-2 mt-6 md:ml-50 cursor-pointer bg-red-600 hover:bg-red-700 text-white font-mono px-4 py-2 rounded-full shadow-lg transition hover:scale-110 hover:shadow-[0_0_30px_#ff0000dd]">
               <span>Learn More</span>
               <span className="text-xl">→</span>
             </button>
--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import SectionTitle from "./Common/SectionTitle";
 
 const Calender = () => {
   return (
     <div>
-      <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-6xl text-center font-serif mt-10 sm:mt-16 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
+      <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-7xl text-center font-serif mt-10 sm:mt-16 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
         Mark Your Calender
       </h2>
       <div
         className="after:content-[''] after:block after:h-[1.5px] after:w-full after:bg-red-600 after:mt-1 before:content-[''] before:block before:h-[1.5px] before:w-full before:bg-red-600 mx-4 sm:mx-8 md:mx-20 lg:mx-100"
       ></div>    
       <div className="flex items-center justify-center mt-6 md:mt-8 text-white overflow-x-auto px-4">
-        <div className="flex flex-nowrap items-center justify-center gap-7 md:space-x-8 lg:space-x-10">
+        <div className="flex flex-nowrap items-center justify-center gap-7 md:space-x-8 lg:space-x-10 sm:mt-10 md:mt-15 ">
           <Timebox value="63" label="Days" />
           <div className="text-red-500 text-xl md:text-5xl font-bold flex flex-col justify-center -mt-2 md:-mt-8">
             :
@@ -29,7 +30,7 @@ const Calender = () => {
 function Timebox(props) {
   return (
     <div className="flex flex-col items-center">
-      <div className="bg-gray-200 text-black rounded-md shadow-[0_0_15px_#ff0000aa] w-16 h-20 md:w-24 md:h-28 lg:w-28 lg:h-34 flex items-center justify-center text-xl md:text-5xl font-bold hover:scale-90 hover:shadow-[0_0_30px_#ff0000dd] cursor-pointer">
+      <div className="bg-gray-200 text-black rounded-md shadow-[0_0_15px_#ff0000aa] w-16 h-20 md:w-24 md:h-28 lg:w-32 lg:h-40 flex items-center justify-center text-xl md:text-6xl font-bold hover:scale-110 hover:shadow-[0_0_30px_#ff0000dd] cursor-pointer">
         {props.value}
       </div>
       <span className="mt-2 text-red-500 font-mono text-xs md:text-3xl">
